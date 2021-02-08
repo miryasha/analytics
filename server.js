f6ef1e92215d8088645960f71c9ef736ed7d08cc
yasha6081 <yasha6081@gmail.con>
2021-02-08 17:57:43 -0600

    sdfds

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+function passwordProtected(req, res, next){
+  res.set('WWW-Authenticate','Basic realm="Dash Board"')
+  //console.log(req.headers.authorization)
+  
+  if (req.headers.authorization == "Basic SkVUWTQ1NzYzJCMmKiMlWUhodmZKREZTRmRndnNkZnNHYTMzNTR5NjQ1Oj8iTExLUEZQR1NkZmdwbzQ5cTh3ZXBxazc4XiUkJCMjZmd1Jmlsbm1LTEs=") {
+    next()
+
+  }else{
+    res.status(401).send("Authentication required")
+  }
+}
+
+app.use(passwordProtected)
 
 // Static directory to be served
 app.use(express.static("./app/public"));
